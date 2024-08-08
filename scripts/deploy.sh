55f71c1d2a0ff38a3aef733af95a72b6d5a420d4
godssun <98688265+godssun@users.noreply.github.com>
2024-08-08 11:41:08 +0900

    "Add GitHub Actions CI pipeline for deploy branch"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 REPOSITORY=/home/ubuntu/spring/project3
 cd $REPOSITORY
 
@@ -11,10 +13,29 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 종료할 프로세스가 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH >/dev/null 2>&1 &
+
+# CodeDeploy 배포
+aws deploy create-deployment \
+  --application-name $CODE_DEPLOY_APP_NAME \
+  --deployment-config-name CodeDeployDefault.OneAtATime \
+  --deployment-group-name $DEPLOYMENT_GROUP_NAME \
+  --s3-location bucket=$BUCKET_NAME,bundleType=zip,key=$PROJECT_NAME/$GITHUB_SHA.zip || {
+    ACTIVE_DEPLOYMENT=$(aws deploy list-deployments --application-name $CODE_DEPLOY_APP_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --include-only-statuses InProgress --query 'deployments[0]' --output text)
+    if [ "$ACTIVE_DEPLOYMENT" != "None" ]; then
+      echo "Stopping active deployment: $ACTIVE_DEPLOYMENT"
+      aws deploy stop-deployment --deployment-id $ACTIVE_DEPLOYMENT --region ap-northeast-2
+      echo "Retrying deployment..."
+      aws deploy create-deployment \
+        --application-name $CODE_DEPLOY_APP_NAME \
+        --deployment-config-name CodeDeployDefault.OneAtATime \
+        --deployment-group-name $DEPLOYMENT_GROUP_NAME \
+        --s3-location bucket=$BUCKET_NAME,bundleType=zip,key=$PROJECT_NAME/$GITHUB_SHA.zip
+    fi
+  }
